ae609d5b2c87a5c4c47fc063557edcef95a5bceb
VasylSamuliak <vasylsamuliak@gmail.com>
2019-03-20 14:15:00 +0200

    move all data classes into "data" package inside "app"

---
+++ app/data/POJO/Category.java
@@ -0,0 +1,53 @@
+package data.POJO;
+
+import  java.util.Date;
+
+public class Category {
+
+    private int id;
+    private String name;
+    private String type;
+    private Date CreatedDate;
+
+    public void setId(int id){
+
+        this.id=id;
+    }
+
+    public int getId(){
+
+        return this.id;
+    }
+
+    public void setName(String name){
+
+        this.name=name;
+    }
+
+    public String getName(){
+
+        return this.name;
+    }
+
+    public void setType(String type){
+
+        this.type=type;
+    }
+
+    public String getType(){
+
+        return this.type;
+    }
+
+    public void setCreatedDate(Date CreatedDate){
+
+        this.CreatedDate=CreatedDate;
+    }
+
+    public Date getCreatedDate(){
+
+        return this.CreatedDate;
+    }
+
+
+}
---
+++ app/data/POJO/Expenses.java
@@ -0,0 +1,56 @@
+package data.POJO;
+
+import java.util.Date;
+
+public class Expenses {
+
+    private int id;
+    private double amount;
+    private Category category;
+    private Date CreatedDate;
+
+    public Expenses(){
+
+        this.amount=0;
+    }
+
+    public void setId(int id){
+
+        this.id=id;
+    }
+
+    public int getId(){
+
+        return this.id;
+    }
+
+    public void setAmount(double amount){
+
+        this.amount=amount;
+    }
+
+    public double getAmount(){
+
+        return this.amount;
+    }
+
+    public void setCategory(Category category){
+
+        this.category=category;
+    }
+
+    public Category getCategory(){
+
+        return this.category;
+    }
+
+    public void setCreatedDate(Date CreatedDate){
+
+        this.CreatedDate=CreatedDate;
+    }
+
+    public Date getCreatedDate(){
+
+        return this.CreatedDate;
+    }
+}
