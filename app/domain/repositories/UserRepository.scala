14eab90abfaeecc59af6d0d4f2f9c28bb26070d4
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2019-04-15 14:04:03 +0300

    add login method

--- app/domain/repositories/UserRepository.scala
+++ app/domain/repositories/UserRepository.scala
@@ -1,7 +1,11 @@
 package domain.repositories
 
 class UserRepository {
+  def login(username: String, password: String): String ={
+    username
+  }
+
   def signUp(username: String, email: String, password: String): String = {
-    username + " " + email + " " + password
+    username
   }
 }
