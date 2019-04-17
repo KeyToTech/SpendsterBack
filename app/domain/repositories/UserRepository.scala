d3e584d2a3b0840314bac387d9c184a80b2bec08
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2019-04-17 11:33:58 +0300

    change repo to return user

--- app/domain/repositories/UserRepository.scala
+++ app/domain/repositories/UserRepository.scala
@@ -1,11 +1,22 @@
 package domain.repositories
 
+import domain.entity.User
+
 class UserRepository {
-  def login(username: String, password: String): String ={
-    username
+  def login(username: String, password: String): User ={
+    //TODO: change this to get actual user
+    val user = new User()
+    user.setUsername(username)
+    user.setPassword(password)
+    return user
   }
 
-  def signUp(username: String, email: String, password: String): String = {
-    username
+  def signUp(username: String, email: String, password: String): User = {
+    //TODO: change this to save user in db and get actual user
+    val user = new User()
+    user.setUsername(username)
+    user.setEmail(email)
+    user.setPassword(password)
+    return user
   }
 }
