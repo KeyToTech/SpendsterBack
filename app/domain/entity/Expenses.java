c658098c73c5748d6d8654da2e640433c75a1f2c
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2019-05-08 15:25:37 +0300

    add note field, change constructor to match it

--- app/domain/entity/Expenses.java
+++ app/domain/entity/Expenses.java
@@ -8,16 +8,18 @@ public class Expenses {
 
     private String id;
     private double amount;
+    private String note;
     private String categoryId;
     private Date CreatedDate;
 
-    public Expenses(double amount, String categoryId){
-        this(randomUUID().toString(), amount, categoryId, new Date());
+    public Expenses(double amount, String note, String categoryId){
+        this(randomUUID().toString(), amount, note, categoryId, new Date());
     }
 
-    public Expenses(String id, double amount, String categoryId, Date CreatedDate) {
+    public Expenses(String id, double amount, String note, String categoryId, Date CreatedDate) {
         this.id = id;
         this.amount = amount;
+        this.note = note;
         this.categoryId = categoryId;
         this.CreatedDate = CreatedDate;
     }
@@ -28,6 +30,10 @@ public class Expenses {
 
     public double getAmount(){
         return this.amount;
+    }
+
+    public String getNote() {
+        return this.note;
     }
 
     public String getCategoryId(){
